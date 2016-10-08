9ae7fd2c99b13352ca3dc21b706f0577ed6896ba
Max <maximberezin97@gmail.com>
2016-10-08 19:35:19 -0400

    Initial commit

---
+++ app/src/main/java/com/berezin/maxim/autouploadfiles/LoginActivity.java
@@ -0,0 +1,19 @@
+package com.berezin.maxim.autouploadfiles;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
+
+    /** Called when the user clicks the Connect button */
+    public void connect(View view) {
+        // Upload the content!
+    }
+}
---
+++ app/src/main/java/com/berezin/maxim/autouploadfiles/MainActivity.java
@@ -0,0 +1,26 @@
+package com.berezin.maxim.autouploadfiles;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+
+public class MainActivity extends AppCompatActivity {
+    public final static String EXTRA_MESSAGE = "com.berezin.maxim.autouploadfiles.MESSAGE";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    /** Called when the user clicks the Send button */
+    public void sendMessage(View view) {
+        Intent intent = new Intent(this, DisplayMessageActivity.class);
+        EditText editText = (EditText) findViewById(R.id.edit_message);
+        String message = editText.getText().toString();
+        intent.putExtra(EXTRA_MESSAGE, message);
+        startActivity(intent);
+    }
+}
