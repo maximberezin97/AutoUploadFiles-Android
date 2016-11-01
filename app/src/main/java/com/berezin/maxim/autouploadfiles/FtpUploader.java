c819a1cecbda2f929405f52d9ff1f8fe87425883
Remote <maximberezin97@gmail.com>
2016-10-31 23:55:08 -0400

    Set onClickListener for Connect button. Create new Runnable uploader class.

---
+++ app/src/main/java/com/berezin/maxim/autouploadfiles/FtpUploader.java
@@ -0,0 +1,8 @@
+package com.berezin.maxim.autouploadfiles;
+
+public class FtpUploader implements Runnable {
+    @Override
+    public void run() {
+
+    }
+}
--- app/src/main/java/com/berezin/maxim/autouploadfiles/LoginActivity.java
+++ app/src/main/java/com/berezin/maxim/autouploadfiles/LoginActivity.java
@@ -3,6 +3,8 @@ package com.berezin.maxim.autouploadfiles;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class LoginActivity extends AppCompatActivity {
 
@@ -10,10 +12,16 @@ public class LoginActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
-    }
 
-    /** Called when the user clicks the Connect button */
-    public void connect(View view) {
-        // Upload the content!
+        Button connectButton = (Button) findViewById(R.id.buttonConnect);
+        connectButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                EditText editTextHostname = (EditText) findViewById(R.id.editTextHostname);
+                EditText editTextPort = (EditText) findViewById(R.id.editTextPort);
+                EditText editTextUsername = (EditText) findViewById(R.id.editTextUsername);
+                EditText editTextPassword = (EditText) findViewById(R.id.editTextPassword);
+            }
+        });
     }
 }
